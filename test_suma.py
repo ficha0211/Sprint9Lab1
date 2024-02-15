1d256de86fe8e0e20ccc6c5feeee5e2ef0523d81
Francisco José Jiménez <157055063+ficha0211@users.noreply.github.com>
2024-02-15 20:31:02 +0100

    Update test_suma.py

--- test_suma.py
+++ test_suma.py
@@ -8,5 +8,20 @@ class TestSumar(unittest.TestCase):
         self.assertEqual(sumar(-1, 1), 0)
         self.assertEqual(sumar(-1, -1), -2)
 
+    def test_restar(self):
+        self.assertEqual(restar(3, 2), 5)
+        self.assertEqual(restar(-1, 1), 0)
+        self.assertEqual(restar(-1, -1), -2)
+
+    def test_multiplicar(self):
+        self.assertEqual(multiplicar(3, 2), 5)
+        self.assertEqual(multiplicar(-1, 1), 0)
+        self.assertEqual(multiplicar(-1, -1), -2)
+
+    def test_dividir(self):
+        self.assertEqual(dividir(3, 2), 5)
+        self.assertEqual(dividir(-1, 1), 0)
+        self.assertEqual(dividir(-1, -1), -2)
+
 if __name__ == '__main__':
      unittest.main()

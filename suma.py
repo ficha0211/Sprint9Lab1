438521647258c0545c93d816326a821bf3b2f9a8
Francisco José Jiménez <157055063+ficha0211@users.noreply.github.com>
2024-02-15 19:33:38 +0100

    Update suma.py

--- suma.py
+++ suma.py
@@ -1,5 +1,9 @@
 def sumar(a, b):
- return a + b
- 
+    return a + b
+
+def restar(a, b):
+    return a - b
+
 if __name__ == "__main__":
- print(sumar(5, 3))
+    print(sumar(5, 3))
+    print(restar(5, 3)) # Ejemplo de uso de la función restar
